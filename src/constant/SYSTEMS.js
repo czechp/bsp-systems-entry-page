788f982f6f7550f139aa7fe369340f438a1cf268
czechp <webcoderc@gmail.com>
2023-06-17 12:10:06 +0200

    Add abbreviation to new utb system

--- src/constant/SYSTEMS.js
+++ src/constant/SYSTEMS.js
@@ -8,6 +8,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 const systems = [
+    {title: "UTB", path: "192.168.6.102:9011", icon: faCarBattery},
     {title: "Zamówienia automatycy", path: "192.168.6.100:9001", icon: faList},
     {title: "MES", path: "192.168.6.100:9005", icon: faChartColumn},
     {title: "WAGI TEALIGHT", path: "192.168.6.94:9001", icon: faScaleBalanced},
