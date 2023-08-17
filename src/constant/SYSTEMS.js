80181cceb2cd49a38247d3571d298d4bb375442e
czechp <webcoderc@gmail.com>
2023-08-17 16:10:00 +0200

    Remove unusued systems

--- src/constant/SYSTEMS.js
+++ src/constant/SYSTEMS.js
@@ -1,7 +1,7 @@
 import {
     faCarBattery,
     faChartColumn,
-    faEnvelope, faList,
+    faList,
     faScaleBalanced,
     faStamp,
     faTemperature4
@@ -13,12 +13,7 @@ const systems = [
     {title: "MES", path: "192.168.6.100:9005", icon: faChartColumn},
     {title: "WAGI TEALIGHT", path: "192.168.6.94:9001", icon: faScaleBalanced},
     {title: "Pomiar temp. laboratorium", path: "192.168.1.71", icon: faTemperature4},
-    {title: "UTB-D", path: "192.168.2.101", icon: faCarBattery},
-    {title: "UTB-AS", path: "192.168.2.115", icon: faCarBattery},
-    {title: "UTB-F", path: "192.168.2.132", icon: faCarBattery},
     {title: "FAMAG", path: "192.168.2.105", icon: faStamp},
-    {title: "EMAIL", path: "192.168.1.35", icon: faEnvelope},
-
 ];
 
 export default systems;
