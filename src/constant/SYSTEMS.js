0449d8ec6d0f5ba2ce9f0af9d71cb34e3e93b874
czechp <webcoderc@gmail.com>
2024-11-16 11:15:43 +0100

    Correct typo in address

--- src/constant/SYSTEMS.js
+++ src/constant/SYSTEMS.js
@@ -10,7 +10,7 @@ import {
 const systems = [
     {title: "UTB", path: "192.168.6.102:9011", icon: faCarBattery},
     {title: "Zamówienia automatycy", path: "bsp-server-2.bispol.pl:9001", icon: faList},
-    {title: "MES", path: "bsp-server-2.bispol:9005", icon: faChartColumn},
+    {title: "MES", path: "bsp-server-2.bispol.pl:9005", icon: faChartColumn},
     {title: "WAGI TEALIGHT", path: "bsp-server-1.bispol.pl:9001", icon: faScaleBalanced},
     {title: "Pomiar temp. laboratorium", path: "192.168.1.71", icon: faTemperature4},
     {title: "FAMAG", path: "192.168.2.105", icon: faStamp},
