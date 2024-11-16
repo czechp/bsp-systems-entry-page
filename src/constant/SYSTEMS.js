1718f299a805cc4e3d0999ea3e5e5d80062abf52
czechp <webcoderc@gmail.com>
2024-11-16 11:07:01 +0100

    New addresses for DNS name

--- src/constant/SYSTEMS.js
+++ src/constant/SYSTEMS.js
@@ -9,9 +9,9 @@ import {
 
 const systems = [
     {title: "UTB", path: "192.168.6.102:9011", icon: faCarBattery},
-    {title: "Zamówienia automatycy", path: "192.168.6.100:9001", icon: faList},
-    {title: "MES", path: "192.168.6.100:9005", icon: faChartColumn},
-    {title: "WAGI TEALIGHT", path: "192.168.6.94:9001", icon: faScaleBalanced},
+    {title: "Zamówienia automatycy", path: "bsp-server-2.bispol.pl:9001", icon: faList},
+    {title: "MES", path: "bsp-server-2.bispol:9005", icon: faChartColumn},
+    {title: "WAGI TEALIGHT", path: "bsp-server-1.bispol.pl:9001", icon: faScaleBalanced},
     {title: "Pomiar temp. laboratorium", path: "192.168.1.71", icon: faTemperature4},
     {title: "FAMAG", path: "192.168.2.105", icon: faStamp},
 ];
