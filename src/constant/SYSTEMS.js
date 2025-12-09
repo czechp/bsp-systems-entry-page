a1dcdb5972ebda5f78edeef5226e47a29dc20fee
PCzech <czechprz@gmail.com>
2025-12-09 10:04:44 +0100

    Update systems list: remove unused icons and add new Wagi AS entry

--- src/constant/SYSTEMS.js
+++ src/constant/SYSTEMS.js
@@ -1,20 +1,16 @@
 import {
     faCarBattery,
-    faChartColumn,
     faList,
     faScaleBalanced,
     faStamp,
-    faTemperature4
 } from "@fortawesome/free-solid-svg-icons";
 
 const systems = [
     {title: "UTB", path: "bsp-server-3.bispol.pl:9011", icon: faCarBattery},
     {title: "Zamówienia automatycy", path: "bsp-server-2.bispol.pl:9001", icon: faList},
-    {title: "MES", path: "bsp-server-2.bispol.pl:9005", icon: faChartColumn},
     {title: "WAGI TEALIGHT", path: "bsp-server-1.bispol.pl:9001", icon: faScaleBalanced},
-    {title: "Pomiar temp. laboratorium", path: "192.168.1.71", icon: faTemperature4},
-    {title: "FAMAG", path: "192.168.2.105", icon: faStamp},
-    {title: "FAMAG - NOWY", path: "bsp-server-4.bispol.pl:9000", icon: faStamp},
+    {title: "FAMAG", path: "bsp-server-4.bispol.pl:9000", icon: faStamp},
+    {title: "Wagi AS", path: "bsp-server-5.bispol.pl:9001", icon: faScaleBalanced}
 ];
 
 export default systems;
