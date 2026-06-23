aac6e7a2194458bb607322b02cdaa948ecd7de3b
PCzech <czechprz@gmail.com>
2026-06-23 08:48:15 +0200

    Update systems list: replace 'Zamówienia automatycy' with 'Rejestracja czasu' and add 'faUserClock' icon

--- src/constant/SYSTEMS.js
+++ src/constant/SYSTEMS.js
@@ -1,13 +1,8 @@
-import {
-    faCarBattery,
-    faList,
-    faScaleBalanced,
-    faStamp,
-} from "@fortawesome/free-solid-svg-icons";
+import {faCarBattery, faScaleBalanced, faStamp, faUserClock,} from "@fortawesome/free-solid-svg-icons";
 
 const systems = [
     {title: "UTB", path: "bsp-server-3.bispol.pl:9011", icon: faCarBattery},
-    {title: "Zamówienia automatycy", path: "bsp-server-2.bispol.pl:9001", icon: faList},
+    {title: "Rejestracja czasu", path: "bsp-server-2.bispol.pl:9001", icon: faUserClock},
     {title: "WAGI TEALIGHT", path: "bsp-server-1.bispol.pl:9001", icon: faScaleBalanced},
     {title: "FAMAG", path: "bsp-server-4.bispol.pl:9000", icon: faStamp},
     {title: "Wagi AS", path: "bsp-server-5.bispol.pl:9001", icon: faScaleBalanced}
