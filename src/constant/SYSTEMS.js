2e41caab771ac867d75f947a514a3857a74e46cf
PCzech <czechprz@gmail.com>
2026-06-25 13:24:04 +0200

    Update systems list: modify path for 'Rejestracja godzin inwestycyjnych'

--- src/constant/SYSTEMS.js
+++ src/constant/SYSTEMS.js
@@ -2,7 +2,7 @@ import {faCarBattery, faScaleBalanced, faStamp, faUserClock,} from "@fortawesome
 
 const systems = [
     {title: "UTB", path: "bsp-server-3.bispol.pl:9011", icon: faCarBattery},
-    {title: "Rejestracja godzin inwestycyjnych", path: "bsp-server-2.bispol.pl:9001", icon: faUserClock},
+    {title: "Rejestracja godzin inwestycyjnych", path: "bsp-server-2.bispol.pl:9001/registered-time/check", icon: faUserClock},
     {title: "WAGI TEALIGHT", path: "bsp-server-1.bispol.pl:9001", icon: faScaleBalanced},
     {title: "FAMAG", path: "bsp-server-4.bispol.pl:9000", icon: faStamp},
     {title: "Wagi AS", path: "bsp-server-5.bispol.pl:9001", icon: faScaleBalanced}
